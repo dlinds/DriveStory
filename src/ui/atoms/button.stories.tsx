168528c011e2c97f7152ffb1a1a291bce8416066
Daniel Lindsey <daniellindsey85254@gmail.com>
2023-03-05 14:04:06 -0800

    add addn stories for button

--- src/ui/atoms/button.stories.tsx
+++ src/ui/atoms/button.stories.tsx
@@ -29,5 +29,37 @@ const ButtonMeta = {
 
 export default ButtonMeta
 
-export const Basic = { variant: 'primary' }
-export const Secondary = { variant: 'secondary' }
+export const Primary = {}
+export const PrimaryDisabled = {
+  title: 'Primary Disabled',
+  args: {
+    disabled: true,
+  },
+}
+export const Secondary = {
+  args: {
+    variant: 'secondary',
+  },
+}
+export const SecondaryDisabled = {
+  title: 'Secondary Disabled',
+  args: {
+    variant: 'sedondary',
+    disabled: true,
+  },
+}
+export const PrimarySpinner = {
+  title: 'Primary Spinner',
+  args: {
+    spinner: true,
+    disabled: true,
+  },
+}
+export const SecondarySpinner = {
+  title: 'Secondary Spinner',
+  args: {
+    spinner: true,
+    variant: 'sedondary',
+    disabled: true,
+  },
+}
