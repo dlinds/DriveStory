3776a9b989bc013449c8e352f6e0a94817d582bc
Daniel Lindsey <daniellindsey85254@gmail.com>
2023-03-05 15:04:45 -0800

    rename atoms folder to _atoms

--- src/ui/Home/screens/Home.tsx
+++ src/ui/Home/screens/Home.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { Dimensions, StyleSheet, Text, View } from "react-native";
-import { Body } from "../molecules/Body";
-import { Header } from "../../atoms/Header";
+import React from 'react'
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
+import { Body } from '../molecules/Body'
+import { Header } from '../../_atoms/Header'
 
 export const Home = () => {
   return (
     <View style={styles.container}>
       <Body />
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-});
+})
