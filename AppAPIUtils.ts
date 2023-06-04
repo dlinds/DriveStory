d59132b2423cb0e4051e851d67fb216f78167a74
Daniel Lindsey <daniellindsey85254@gmail.com>
2023-06-03 17:02:23 -0700

    strip down chatgpt expectations, simple story only

--- AppAPIUtils.ts
+++ AppAPIUtils.ts
@@ -1,9 +1,7 @@
 import Config from 'react-native-config'
 import RNFS from 'react-native-fs'
-import Sound from 'react-native-sound'
 import { handleSaveFileToDevice } from './AppStorageUtils'
 import { Configuration, OpenAIApi } from 'openai'
-import Voice from '@react-native-voice/voice'
 import { Store } from './AppStateMutate'
 
 export const handleGenerateAndSaveStory = async (
@@ -72,57 +70,6 @@ const configuration = new Configuration({
 
 const openAiConfig = new OpenAIApi(configuration)
 
-interface ExpectedJSONResponse {
-  readonly title: string
-  readonly characters: { readonly name: string }[]
-  readonly story: TwoCharacterResponse[]
-}
-
-const formatJSONToSSML = (json: string): string => {
-  const parsedJSON: ExpectedJSONResponse = JSON.parse(json)
-
-  const { characters, story } = parsedJSON
-
-  const characterNames = characters.map((character) => character.name)
-
-  const storyText = story.map((story) => {
-    const { voice, text } = story
-    return `<speak><voice name="${voice}">${text}</voice></speak>`
-  })
-
-  const storyTextString = storyText.join(' ')
-
-  return `<speak><p>${characterNames.join(' ')} ${storyTextString}</p></speak>`
-}
-
-const constructPrompt = (store: Store | undefined, prompt: string): string => {
-  if (!store)
-    return `Tell me a children's story that includes the topic: ${prompt}`
-  const storeText =
-    store.selectedCustomizedOptions && store.selectedCustomizedOptions[0].label
-
-  console.log({ storeText })
-
-  console.log({ store, selected: store.selectedCustomizedOptions })
-
-  if (storeText) {
-    switch (storeText) {
-      case 'Two Characters':
-        return `Tell me a JSON children's story about ${prompt} with narrator, two characters, 'voice' field (speaker) and 'text' field (dialogue)`
-      case 'Chapters':
-        return `Tell me a childrens story about ${prompt} that takes 10 replies to tell`
-      default:
-        return `Tell me a children's story that includes the topics: ${prompt}`
-    }
-  }
-  return `Tell me a children's story that includes the topics: ${prompt}`
-}
-
-interface TwoCharacterResponse {
-  readonly voice: string
-  readonly text: string
-}
-
 export const queryOpenAi = async ({
   prompt,
   max_tokens = 2500,
@@ -130,15 +77,15 @@ export const queryOpenAi = async ({
   model = models.davinci3,
   store,
 }: queryOpenAiProps) => {
-  const generatedPrompt = constructPrompt(store, prompt)
-  console.log({ generatedPrompt })
+  // const generatedPrompt = constructPrompt(store, prompt)
+  // console.log({ generatedPrompt })
   try {
     const resp = await openAiConfig.createChatCompletion({
       model,
       messages: [
         {
           role: 'user',
-          content: generatedPrompt,
+          content: prompt,
         },
       ],
       max_tokens,
